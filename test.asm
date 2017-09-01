4c6637bfed1994c08524c4a92109d616a038de3a
Vecnik <Ve_cni_k@inbox.ru>
2017-09-01 22:07:06 +0700

    os

--- test.asm
+++ test.asm
@@ -1,5 +1,28 @@
-loop:
-	jmp loop;
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'A'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 't'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'n'
+int 0x10
 
-times 510-($-$$) db 0
+loop:
+	jmp loop
+
+times 510 -($-$$) db 0
 dw 0xaa55

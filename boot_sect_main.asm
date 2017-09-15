8b9568928fa8eba8ebc2322a5cba3d1f97c5f3db
Vecnik <Ve_cni_k@inbox.ru>
2017-09-15 23:37:09 +0700

    os

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,28 +1,29 @@
 [org 0x7c00]
+mov bp, 0x8000
+mov sp, bp
 
-mov bx, HELLO
-call print
+mov bx, 0x9000
+mov dh, 2
+
+call disk_load
+mov dx, [0x9000]
+call print_hex
 
 call print_nl
-
-mov bx, GOODBYE
-call print
-
+mov dx, [0x9000 + 512]
 call print_nl
 
-mov dx, 0x12fe
+mov dx, [0x9000 + 512]
 call print_hex
 
 jmp $
 
 %include "boot_sect_print.asm"
 %include "boot_sect_print_hex.asm"
-
-HELLO:
-	db 'Hello, World', 0
-
-GOODBYE:
-	db 'Goodbye', 0
+%include "boot_sect_disk.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface

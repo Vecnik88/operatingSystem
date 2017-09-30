86b2b9e000ff5339865db69629f37b8ee86cf946
Vecnik <Ve_cni_k@inbox.ru>
2017-09-30 09:20:03 +0700

    os

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,38 +4,6 @@
 #include "../drivers/ports.h"
 
 void main() {
-	/*monitor_clear();
-	monitor_write("kernel run\n");
-	isr_init();
-
-	__asm__ __volatile__("int $2");
-	__asm__ __volatile__("int $3");*/
-	    /* Screen cursor position: ask VGA control register (0x3d4) for bytes
-     * 14 = high byte of cursor and 15 = low byte of cursor. */
-    outb(0x3d4, 14); /* Requesting byte 14: high byte of cursor pos */
-    /* Data is returned in VGA data register (0x3d5) */
-    int position = inb(0x3d5);
-    position = position << 8; /* high byte */
-
-    outb(0x3d4, 15); /* requesting low byte */
-    position += inb(0x3d5);
-
-    /* VGA 'cells' consist of the character and its control data
-     * e.g. 'white on black background', 'red text on white bg', etc */
-    int offset_from_vga = position * 2;
-
-    /* Now you can examine both variables using gdb, since we still
-     * don't know how to print strings on screen. Run 'make debug' and
-     * on the gdb console:
-     * breakpoint kernel.c:21
-     * continue
-     * print position
-     * print offset_from_vga
-     */
-
-    /* Let's write on the current cursor position, we already know how
-     * to do that */
-    char *vga = 0xb8000;
-    vga[offset_from_vga] = 'X'; 
-    vga[offset_from_vga+1] = 0x0f; 
+	clear_monitor();
+	k_print("Hello Anton\n");
 }

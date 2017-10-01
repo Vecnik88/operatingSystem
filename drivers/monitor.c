9c6a352a745a287405b6e88b240ae37d87c99bb2
Vecnik <Ve_cni_k@inbox.ru>
2017-10-01 09:07:12 +0700

    os

--- drivers/monitor.c
+++ drivers/monitor.c
@@ -1,24 +1,23 @@
 #include "monitor.h"
 #include "ports.h"
 
-u32_int get_cursor_offset();
-void set_cursor_offset(u32_int offset);
-u32_int print_char(s8_int c, u32_int col, u32_int row, s8_int attr);
-u32_int get_offset(u32_int col, u32_int row);
-u32_int get_offset_row(u32_int offset);
-u32_int get_offset_col(u32_int offset);
+s32_int get_cursor_offset();
+void set_cursor_offset(s32_int offset);
+s32_int print_char(s8_int c, s32_int col, s32_int row, s8_int attr);
+s32_int get_offset(s32_int col, s32_int row);
+s32_int get_offset_row(s32_int offset);
+s32_int get_offset_col(s32_int offset);
 
 /* печатает строку в соответствующих местах */
 void k_print_at(s8_int* str, s32_int col, s32_int row)
 {
-	u32_int offset;
+	s32_int offset;
 	if (col >= 0 && row >= 0) {
 		offset = get_offset(col, row);
 	} else {
 		offset = get_cursor_offset();
 		row = get_offset_row(offset);
 		col = get_offset_col(offset);
-		//k_print_at("I run", 5, 5);
 	}
 
 	u32_int i = 0;
@@ -34,21 +33,21 @@ void k_print(s8_int* str)
 	k_print_at(str, -1, -1);
 }
 
-u32_int print_char(s8_int c, u32_int col, u32_int row, s8_int attr)
+s32_int print_char(s8_int c, s32_int col, s32_int row, s8_int attr)
 {
-	u8_int* video_memory = (u8_int*)VIDEO_ADDRESS_VGA;
+	s8_int* video_memory = (s8_int*)VIDEO_ADDRESS_VGA;
 
 	if (!attr)
 		attr = WHITE_ON_BLACK;
 
-	if (col >= MAX_COLS || row > MAX_ROWS) {
+	if (col >= MAX_COLS || row >= MAX_ROWS) {
 		video_memory[2*(MAX_COLS)*(MAX_ROWS) - 2] = 'E';
 		video_memory[2*(MAX_COLS)*(MAX_ROWS) - 1] = RED_ON_WHITE;
 
 		return get_offset(col, row);
 	}
 
-	u32_int offset;
+	s32_int offset;
 	if (col >= 0 && row >= 0)
 		offset = get_offset(col, row);
 	else
@@ -67,17 +66,17 @@ u32_int print_char(s8_int c, u32_int col, u32_int row, s8_int attr)
 	return offset;
 }
 
-u32_int get_cursor_offset()
+s32_int get_cursor_offset()
 {
 	out_byte(VGA_CTRL, 14);
-	u32_int offset = in_byte(VGA_DATA) << 8;
+	s32_int offset = in_byte(VGA_DATA) << 8;
 	out_byte(VGA_CTRL, 15);
 	offset += in_byte(VGA_DATA);
 
 	return offset*2;
 }
 
-void set_cursor_offset(u32_int offset)
+void set_cursor_offset(s32_int offset)
 {
 	offset /= 2;
 	out_byte(VGA_CTRL, 14);
@@ -100,16 +99,16 @@ void clear_screen()
 	set_cursor_offset(get_offset(0,0));
 }
 
-u32_int get_offset(u32_int col, u32_int row)
+s32_int get_offset(s32_int col, s32_int row)
 {
 	return (2 * (row * MAX_COLS + col));
 }
 
-u32_int get_offset_row(u32_int offset)
+s32_int get_offset_row(s32_int offset)
 {
 	return (offset/(2*MAX_COLS));
 }
-u32_int get_offset_col(u32_int offset)
+s32_int get_offset_col(s32_int offset)
 {
 	return (offset-(get_offset_row(offset)*2*MAX_COLS))/2;
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,6 +3,10 @@
 /* start kernel */
 void main(void)
 {
-	clear_screen();
-	k_print("Hello! I am your kernel\n");
+ 	clear_screen();
+    k_print_at("X", 1, 6);
+    k_print_at("This text spans multiple lines", 75, 10);
+    k_print_at("There is a line\nbreak", 0, 20);
+    k_print("There is a line\nbreak");
+    k_print_at("What happens when we run out of space?", 45, 24);
 }

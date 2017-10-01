d22673f8eb1fdc5e9e6c413da36d94b9c8c8e1fa
Vecnik <Ve_cni_k@inbox.ru>
2017-10-01 22:52:59 +0700

    os

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,34 +1,26 @@
-#include "../drivers/ports.h"
+#include "../cpu/isr.h"
 #include "../drivers/monitor.h"
+#include "kernel.h"
+#include "../libc/string.h"
 
 /* start kernel */
 void main(void)
 {
  	clear_screen();
  	isr_init();
+ 	irq_init();
 
-    asm volatile("sti");
-    init_timer(50);
-    /* Comment out the timer IRQ handler to read
-     * the keyboard IRQs easier */
-    init_keyboard();
+ 	k_print("HEllo");
 
-    /* Comment out the timer IRQ handler to read
-     * the keyboard IRQs easier 
+}
 
-    k_print_at("X", 1, 6);
-    k_print_at("This text spans multiple lines", 75, 10);
-    k_print_at("There is a line\nbreak", 0, 20);
-    k_print("There is a line\nbreak");
-    k_print_at("What happens when we run out of space?", 45, 24);
-    isr_init();
-    // Test the interrupts 
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
-    __asm__ __volatile__("int $4");
-    __asm__ __volatile__("int $5");
-    __asm__ __volatile__("int $6");
-    __asm__ __volatile__("int $7");
-    __asm__ __volatile__("int $8");
-    __asm__ __volatile__("int $9");*/
+void user_input(s8_int* input)
+{
+    if (strcmp(input, "END") == 0) {
+        k_print("Stopping the CPU. Bye!\n");
+        asm volatile("hlt");
+    }
+    k_print("You said: ");
+    k_print(input);
+    k_print("\n> ");
 }
---
+++ kernel/kernel.h
@@ -0,0 +1,8 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include "../cpu/types.h"
+
+void user_input(s8_int* input);
+
+#endif

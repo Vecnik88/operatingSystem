58287bf3fa318faa8e5939990693e4d5ccf30f31
Vecnik <Ve_cni_k@inbox.ru>
2017-10-07 11:44:32 +0700

    os

--- cpu/idt.h
+++ cpu/idt.h
@@ -7,13 +7,20 @@
 #define IDT_ENTRIES 256
 
 typedef struct {
-	u16_int low_offset;
-	u16_int sel;
-	u8_int always0;
+	u16_int low_offset;					/* младшие 16 бит адреса функции обработчика */
+	u16_int sel;						/* селектор ядра */
+	u8_int always0;						/* всегда 0 */
+	/* флаги:
+	 * 7 бит "если установлен, прерывание присутствует"
+	 * 5-6 бит "уровень привилегий(0=максимальный, 3=минимальный)"
+	 * 4 бит "0 для прерываний"
+	 * 0-3 бит "1110 - 32 битное прерывание"
+	 */
 	u8_int flags;
-	u16_int high_offset;
+	u16_int high_offset;				/* старшие 16 бит адреса функции обработчика */
 } __attribute__((packed)) idt_gate_t;
 
+/* указатель на массив обработчиков прерываний */
 typedef struct {
 	u16_int limit;
 	u32_int base;

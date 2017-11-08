46c7577beb201d9bebfc273504cc9ccad6cee756
Vecnik <Ve_cni_k@inbox.ru>
2017-11-09 00:58:52 +0700

    makefile refresh

--- cpu/ports.h
+++
@@ -1,6 +0,0 @@
-#include "../cpu/types.h"
-
-u8_int in_byte(u16_int port);					/* получаем байт из порта */
-void out_byte(u16_int port, u8_int data);		/* отправляем байт в порт */
-u16_int in_word(u16_int port);					/* получаем 2-байта из порта */
-void out_word(u16_int port, u16_int data);		/* отправляем 2-байта в порт */
--- libc/mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-#include "../cpu/types.h"
-
-void mem_cpy(s8_int* src, s8_int* dst, u32_int count);
-void mem_set(s8_int* dst, u8_int value, u32_int len);
-
-#endif

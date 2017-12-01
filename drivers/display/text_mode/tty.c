edf1120595ac8f8fbb7aa65818ab8f0818f2ce13
Vecnik <Ve_cni_k@inbox.ru>
2017-12-02 01:02:52 +0700

    [drivers] add command to shell

--- drivers/display/text_mode/tty.c
+++ drivers/display/text_mode/tty.c
@@ -1,12 +1,56 @@
 #include <AOS-unix/printk.h>
 #include <AOS-unix/string.h>
 #include <AOS-unix/keyboard.h>
+#include <AOS-unix/cpuid.h>
 
 char tty_buffer[1024];
+char **command = {
+	"END", 
+	"CPUID",
+	"ECHO"
+};
+
+static void computer_off()
+{
+	asm volatile("cli");
+	asm volatile("hlt");
+}
+
+void echo_command(char *str)
+{
+	int i = 5;
+	while(str[i])
+		monitor_put_char(str[i++]);
+}
+
+void incorrect_cmd()
+{
+	printk("\n Incorrect command\n");
+}
 
 static void run_command(char *str)
 {
-	printk("\nYour message: %s\n", str);
+	int i, j;
+	for (i = 0; i < sizeof(command); ++i) {
+		for (j = 0; j < sizeof(str); ++j) {
+			if (str[j] == command[i][j]) {
+				while(str[j]) {
+					if (str[j] == command[i][j++])
+						continue;
+					else
+						break;
+				}
+				switch(i) {
+					case 0: computer_off(); return;
+					case 1: print_info_cpu(); return;
+					case 2: echo_command(str); return;
+					default: incorrect_cmd(); return;
+				}
+			} else {
+				break;
+			}
+		}
+	}
 }
 
 void shell(char symbol, uint8_t scancode)

3c5c4412537ad69dd29d989a687267bde7184e2f
Vecnik <Ve_cni_k@inbox.ru>
2017-12-02 11:43:07 +0700

    [drivers] add tty module with commands

--- drivers/display/text_mode/tty.c
+++ drivers/display/text_mode/tty.c
@@ -4,53 +4,68 @@
 #include <AOS-unix/cpuid.h>
 
 char tty_buffer[1024];
-char **command = {
-	"END", 
-	"CPUID",
-	"ECHO"
-};
+
+static int is_echo_cmd(char *str)
+{
+	int i = 0;
+	if (str[i++] == 'E')
+		if (str[i++] == 'C')
+			if (str[i++] == 'H')
+				if (str[i++] == 'O')
+					if (str[i] == ' ')
+						return 1;
+
+	return 0;
+}
 
 static void computer_off()
 {
+	printk("computer_off");
 	asm volatile("cli");
 	asm volatile("hlt");
 }
 
-void echo_command(char *str)
+static void echo_command(char *str)
 {
+	monitor_put_char('\n');
+
 	int i = 5;
 	while(str[i])
 		monitor_put_char(str[i++]);
+
+	monitor_put_char('\n');
 }
 
-void incorrect_cmd()
+static void incorrect_cmd()
 {
 	printk("\n Incorrect command\n");
 }
 
+static void usage()
+{
+	printk("\nOS Development Series Keyboard Programming Demo");
+	printk("\nwith a basic Command Line Interface (CLI)\n\n");
+	printk("Supported commands:\n");
+	printk(" - END: quits and halts the system\n");
+	printk(" - CLS: clears the display\n");
+	printk(" - HELP: displays this message\n");
+	printk(" - CPUID: info with cpu\n");
+}
+
 static void run_command(char *str)
 {
-	int i, j;
-	for (i = 0; i < sizeof(command); ++i) {
-		for (j = 0; j < sizeof(str); ++j) {
-			if (str[j] == command[i][j]) {
-				while(str[j]) {
-					if (str[j] == command[i][j++])
-						continue;
-					else
-						break;
-				}
-				switch(i) {
-					case 0: computer_off(); return;
-					case 1: print_info_cpu(); return;
-					case 2: echo_command(str); return;
-					default: incorrect_cmd(); return;
-				}
-			} else {
-				break;
-			}
-		}
-	}
+	if (strcmp(str, "CPUID") == 0)
+		print_info_cpu();
+	else if (is_echo_cmd(str))
+		echo_command(str);
+	else if (strcmp(str, "HELP") == 0)
+		usage();
+	else if (strcmp(str, "CLS") == 0)
+		monitor_clear();
+	else if (strcmp(str, "END") == 0)
+		computer_off();
+	else
+		incorrect_cmd();
 }
 
 void shell(char symbol, uint8_t scancode)
@@ -59,10 +74,12 @@ void shell(char symbol, uint8_t scancode)
 
 	if (scancode == ENTER) {
 		tty_buffer[count] = '\0';
-		run_command(tty_buffer);
+		if (count != 0)
+			run_command(tty_buffer);
+		
 		memset(tty_buffer, '\0', sizeof(tty_buffer));
 		count = 0;
-		printk("AOS-unix_root> ");
+		printk("\nAOS-unix_root> ");
 	} else if (scancode == BACKSPACE && count) {
 			monitor_backspace();
 			--count;

fa4358c9262c9a8cb82358a1ed1f58eeae8295fc
Vecnik <Ve_cni_k@inbox.ru>
2017-12-02 11:42:18 +0700

    [libc] add strcmp

--- include/AOS-unix/string.h
+++ include/AOS-unix/string.h
@@ -7,5 +7,6 @@ size_t strlen(const char *str);
 void *memcpy(void *dst, const void *src, size_t size);
 void *memset(void *dst, int fill, size_t size);
 void itoa(int n, char s[]);
+int strcmp(char *str1, char *str2);
 
 #endif /*__STRING_H__*/
--- libc/string.c
+++ libc/string.c
@@ -9,6 +9,20 @@ size_t strlen(const char *str)
 	return str - begin - 1;
 }
 
+int strcmp(char *str1, char *str2)
+{
+    if (!str1 || !str2)
+        return -1;
+    
+    int i;
+    for (i = 0; i < strlen(str1); ++i)
+        if (str1[i] == str2[i])
+            continue;
+        else return -1;
+
+    return 0;
+}
+
 void *memcpy(void *dst, const void *src, size_t size)
 {
 	char *to = dst;
